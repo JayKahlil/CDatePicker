cc088557a021bccfa9a4c1d1f3ac9f4b773f4256
Jay Hussaini <kahlil@btinternet.com>
2019-05-22 01:28:31 +0100

    Oops! forgot to include files for carthage.

---
+++ Carthage/Build/iOS/CDatePicker.framework/Headers/CDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  CDatePicker.h
+//  CDatePicker
+//
+//  Created by Hyder Hussaini on 20/05/2019.
+//  Copyright © 2019 Jay Hussaini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CDatePicker.
+FOUNDATION_EXPORT double CDatePickerVersionNumber;
+
+//! Project version string for CDatePicker.
+FOUNDATION_EXPORT const unsigned char CDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CDatePicker/PublicHeader.h>
+
+
